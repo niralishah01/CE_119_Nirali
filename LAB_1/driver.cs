8f0bcee06c65fb11b0c6977ac49f570aa38072a4
niralishah01 <niralipshah2000@gmail.com>
2020-08-01 00:09:12 +0530

    driver program file refers to addition.dll is added

---
+++ LAB_1/driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using L2t1ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class driver
+    {
+        static void Main(string[] args)
+        {
+			addition a = new addition();
+			int ans = a.sum(2, 4);
+			Console.WriteLine(ans);
+			Console.ReadKey();
+        }
+    }
+}
