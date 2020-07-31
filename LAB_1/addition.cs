dc5e828f54cfb00e3154a1d8503f5dfd82645a65
niralishah01 <niralipshah2000@gmail.com>
2020-08-01 00:03:23 +0530

    file contains an additional functional to create dll is added

---
+++ LAB_1/addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2t1ClassLibrary1
+{
+    public class addition
+    {
+        public int sum(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
+
