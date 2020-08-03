82e01727aba20a34b9ef048300a435dcfb32a85a
niralishah01 <68777758+niralishah01@users.noreply.github.com>
2020-08-04 00:20:24 +0530

    Add files via upload

---
+++ LAB_4/Image.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Image : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            if(Image1.Visible)
+            {
+                Image1.Visible = false;
+            }
+            else 
+            {
+                Image1.Visible = true;
+            }
+        }
+    }
+}
