1e002ac9d1eff2c85921938f77a2de4362fe645d
niralishah01 <niralipshah2000@gmail.com>
2020-08-03 22:02:22 +0530

    Login.aspx.cs added

---
+++ LAB_4/Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string uname = TextBox1.Text;
+            string password = TextBox2.Text;
+            if(uname=="admin")
+            {
+                if(password =="admin")
+                {
+                    Server.Transfer("Home.aspx");
+                }
+                else
+                {
+                    Label1.Text = "Incorrect Credentials";
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            else
+            {
+                Label1.Text = "Incorrect Credentials";
+            }
+        }
+    }
+}
