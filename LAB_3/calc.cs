fc2b73d7e27739470f0109fc8c96264f7e61d2ff
niralishah01 <niralipshah2000@gmail.com>
2020-08-09 02:38:31 +0530

    calc.cs added

---
+++ LAB_3/calc.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace calc_prog
+{
+	public class calc_prog_class
+	{
+		public int addition(int num1,int num2)
+		{
+			return num1+num2;
+		}
+		public int multiplication(int num1,int num2)
+		{
+			return num1*num2;
+		}
+	}
+}
