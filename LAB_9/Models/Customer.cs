484950c62f2ec6beef2258137a7eb9572a597e11
niralishah01 <68777758+niralishah01@users.noreply.github.com>
2020-11-03 22:38:54 +0530

    Models are added

---
+++ LAB_9/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wddn_lab9.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(12,ErrorMessage ="Customer name can't be more than 12 chars.")]
+        public string name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email Address")]
+        public string email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}$",ErrorMessage ="Mobile no should contain exactly 10 digits")]
+        public decimal Mobile_no { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ LAB_9/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wddn_lab9.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public int Cust_ID { get; set; }
+        public Customer Customer { get; set; }
+
+        public int P_ID { get; set; }
+        public Product Product { get; set; }
+
+        [Range(0,100000.00)]
+        public decimal Amount { get; set; }
+        public DateTime Order_datetime { get; set; }
+    }
+}
