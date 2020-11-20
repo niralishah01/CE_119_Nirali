abf3f87bc03ee8a164d277ba954c62ba96698722
niralishah01 <niralipshah2000@gmail.com>
2020-11-20 23:40:55 +0530

    files are added

---
+++ LAB_9/Models/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using wddn_lab9.Models;
+
+namespace wddn_lab9.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        /* Domain classes */
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        /* Configuring Joining table properties */
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Order>()
+                .HasKey(t => new { t.ID });
+
+            /*builder.Entity<Order>()
+                .Property(pt => pt.Amount);
+
+            builder.Entity<Order>()
+                .Property(pt => pt.Order_datetime);*/
+
+            builder.Entity<Order>()
+                .HasOne<Customer>(c => c.Customer)
+                .WithMany(o => o.Orders)
+                .HasForeignKey(o => o.Cust_ID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Order>()
+                .HasOne<Product>(p => p.Product)
+                .WithMany(o => o.Orders)
+                .HasForeignKey(o => o.P_ID);
+
+        }
+    }
+}
---
+++ LAB_9/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wddn_lab9.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [DataType("ntext")]
+        public string details { get; set; }
+
+        [Required]
+        [Range(0,100000.00)]
+        public decimal price { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
